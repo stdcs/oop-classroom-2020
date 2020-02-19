558b057df3428c39c8b4a3bb67ce560f9a484f8f
Arsyi Aziz <azizarsyi@gmail.com>
2020-02-20 07:10:35 +0800

    Removed Random Faculty and Major Names | You're a wizard Harry 🧙‍♂️

--- assigntment-01/H071191003/Main.java
+++ assigntment-01/H071191003/Main.java
@@ -13,20 +13,20 @@ public class Main {
     public static void main(String[] args) {
         Map<String, String> facultyMap = new HashMap<>();
         Map<String, String> majorMap = new HashMap<>();
-        facultyMap.put("Faculty of Magic", "A");
-        facultyMap.put("Faculty of Sorcery", "B");
-        facultyMap.put("Faculty of Witchcraft", "C");
-        facultyMap.put("Faculty of Muggle Sciences", "D");
-        facultyMap.put("Faculty of Wizardry", "E");
-        facultyMap.put("Random Faculty 1", "F");
-        facultyMap.put("Random Faculty 2", "G");
+        facultyMap.put("Fakultas Ekonomi dan Bisnis", "A");
+        facultyMap.put("Fakultas Hukum", "B");
+        facultyMap.put("Fakultas Kedokteran", "C");
+        facultyMap.put("Fakultas Teknik", "D");
+        facultyMap.put("Fakultas Ilmu Sosial dan Ilmu Politik", "E");
+        facultyMap.put("Fakultas Ilmu Budaya", "F");
+        facultyMap.put("Fakultas Pertanian", "G");
         facultyMap.put("MIPA", "H");
-        majorMap.put("Voldemort", "01");
-        majorMap.put("Does", "02");
-        majorMap.put("Not", "03");
-        majorMap.put("Have", "04");
-        majorMap.put("A", "05");
-        majorMap.put("Nose", "06");
+        majorMap.put("Matematika", "01");
+        majorMap.put("Fisika", "02");
+        majorMap.put("Kimia", "03");
+        majorMap.put("Biologi", "04");
+        majorMap.put("Statistika", "05");
+        majorMap.put("Geofisika", "06");
         majorMap.put("Ilmu Komputer", "07");
 
         Student student1 = new Student();
@@ -45,9 +45,9 @@ public class Main {
         student2.setFirstName("haRrY");
         student2.setLastName("PoTTer");
         student2.setRegisterYear(1750);
-        student2.setFaculty("Faculty of Muggle Sciences");
-        student2.setDepartment("Muggle Studies");
-        student2.setMajor("Voldemort");
+        student2.setFaculty("MIPA");
+        student2.setDepartment("Biologi");
+        student2.setMajor("Biologi");
         student2.setId(facultyMap, majorMap);
         student2.setEmail(facultyMap);
 
@@ -55,8 +55,8 @@ public class Main {
         student3.setLastName("Musk");
         student3.setRegisterYear(2000);
         student3.setFaculty("MIPA");
-        student3.setDepartment("Matematika");
-        student3.setMajor("Ilmu Komputer");
+        student3.setDepartment("Geofisika");
+        student3.setMajor("Geofisika");
         student3.setId(facultyMap, majorMap);
         student3.setEmail(facultyMap);
 
