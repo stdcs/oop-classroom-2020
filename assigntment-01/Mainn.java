e1498ee4482b44ef048cf31902bb755abf0cf982
indiraseptianita <you@example.com>
2020-02-21 15:16:07 +0800

    Assigment 01

---
+++ assigntment-01/Mainn.java
@@ -0,0 +1,62 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Mainn {
+
+    public static void main(String[] args) {
+        Map<String, String> facultyMap = new HashMap<>();
+        Map<String, String> majorMap = new HashMap<>();
+        facultyMap.put("Farmasi", "A");
+        facultyMap.put("Pertanian", "B");
+        facultyMap.put("Kedokteran", "C");
+        facultyMap.put("Kehutanan", "D");
+        facultyMap.put("Ekonomi dan Bisnis", "E");
+        facultyMap.put("Ilmu Budaya", "F");
+        facultyMap.put("Ilmu Sosial dan Politik", "G");
+        facultyMap.put("MIPA", "H");
+        
+        majorMap.put("Matematika", "01");
+        majorMap.put("Statistika", "02");
+        majorMap.put("Fisika", "03");
+        majorMap.put("Geofisika", "04");
+        majorMap.put("Biologi", "05");
+        majorMap.put("Kimia", "06");
+        majorMap.put("Ilmu Komputer", "07");
+
+        Student student1 = new Student();
+        Student student2 = new Student();
+        Student student3 = new Student();
+
+        student1.setFirstName("mUHammAd");
+        student1.setLastName("fITRAH");
+        student1.setRegisterYear(2017);
+        student1.setFaculty("MIPA");
+        student1.setDepartment("Matematika");
+        student1.setMajor("Ilmu Komputer");
+        student1.setId(facultyMap, majorMap);
+        student1.setEmail(facultyMap);
+
+        student2.setFirstName("KENNEDY");
+        student2.setLastName(" ");
+        student2.setRegisterYear(2017);
+        student2.setFaculty("MIPA");
+        student2.setDepartment("Matematika");
+        student2.setMajor("Ilmu Komputer");
+        student2.setId(facultyMap, majorMap);
+        student2.setEmail(facultyMap);
+
+        student3.setFirstName("Khawaritzmi");
+        student3.setLastName("abdallah ahmad");
+        student3.setRegisterYear(2017);
+        student3.setFaculty("MIPA");
+        student3.setDepartment("Matematika");
+        student3.setMajor("Ilmu Komputer");
+        student3.setId(facultyMap, majorMap);
+        student3.setEmail(facultyMap);
+
+        student1.desc();
+        student2.desc();
+        student3.desc();
+
+    }
+} 
---
+++ assigntment-01/Student1.java
@@ -0,0 +1,92 @@
+import java.util.Map;
+import java.util.Random;
+
+class Student1{
+
+    private String id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private int registerYear;
+    private String faculty;
+    private String department;
+    private String major;
+
+    public String getId(){
+        return id;
+    }
+    public void setId(String id){
+        this.id = id;
+    }
+    public void setId(Map <String, String> facultyMap, Map <String, String> majorMap){
+        String facultyCode = facultyMap.get(faculty);
+        String majorCode = majorMap.get(major);
+        int ldregister = registerYear % 100;
+        Random rdnum = new Random();
+        int rdnum1 = rdnum.nextInt(60) + 1;
+        id = String.format("%s%s1%02d%03d", facultyCode, majorCode, ldregister, rdnum1);
+
+    }
+    public void setFirstName(String firstName){
+        this.firstName = firstName;
+    }
+    public void setLastName(String lastName){
+        this.lastName = lastName;
+    }
+    public void setEmail(String email){
+        this.email = email;
+    }
+    //BEHAVOUR
+    public void setEmail(Map<String, String> facultyMap){
+        String fullname = (firstName + " " + lastName).toLowerCase();
+        String[] nameByWord = fullname.split(" ");
+        email = nameByWord[nameByWord.length - 1];
+        for (int i = 0; i < nameByWord.length - 1; i++) {
+            email += nameByWord[i].charAt(0);
+        }
+        email += (registerYear % 100) + facultyMap.get(faculty).toLowerCase() + "@student.unhas.ac.id";
+    }
+    public String getEmail(){
+        return email;
+    }
+    public void setRegisterYear(int registerYear){
+        this.registerYear = registerYear;
+    }
+    public void setFaculty(String faculty){
+        this.faculty = faculty;
+    }
+    public String getFaculty(){
+        return faculty;
+    }
+    public void setDepartment(String department){
+        this.department = department;
+    }
+    public String getDepartment(){
+        return department;
+    }
+    public void setMajor(String major){
+        this.major = major;
+    }
+    public String getMajor(){
+        return major;
+    }
+    public String getFullName(){
+        String fullName = (firstName + " " + lastName).toLowerCase();
+        String[] splitName = fullName.split(" ");
+        String finalName = "";
+        for (int i = 0; i < splitName.length; i++){
+            finalName += splitName[i].substring(0, 1).toUpperCase() + splitName[i].substring(1, splitName[i].length()) + " ";
+        }
+        return finalName;
+    }
+
+    public void description (){
+        System.out.println("Nama            : " + getFullName());
+        System.out.println("NIM             : " + getId());
+        System.out.println("Email Mahasiswa : " + getEmail());
+        System.out.println("Fakultas        : " + getFaculty());
+        System.out.println("Department      : " + getDepartment());
+        System.out.println("Program Studi   : " + getMajor());
+        System.out.println();
+    }
+}
