560ff9fbfbeada64a6fae0856acbc09dcd9d847a
Arsyi Aziz <azizarsyi@gmail.com>
2020-03-04 23:12:58 +0800

    Assignment 2 Submission 🐥

---
+++ assignment-02/H071191003/UserDetail.java
@@ -0,0 +1,30 @@
+public class UserDetail {
+    //This class contains the details of a user
+    private Integer id;
+    private String name;
+    private String dateOfBirth;
+    private String email;
+
+    public UserDetail(Integer id, String name, String dateOfBirth, String email) {
+        this.id = id;
+        this.name = name;
+        this.dateOfBirth = dateOfBirth;
+        this.email = email;
+    }
+    public Integer getId() {
+        return id;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+    
+    public String getEmail() {
+        return email;
+    }
+    
+}
