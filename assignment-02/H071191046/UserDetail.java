6abf8469c1108255be60799984859ee30816bdbd
SakinahYunus <sakinahys205@gmail.com>
2020-03-06 08:05:39 -0500

    Tugas 2

---
+++ assignment-02/H071191046/UserDetail.java
@@ -0,0 +1,29 @@
+public class UserDetail {
+
+    private Integer id;
+    private String name;
+    private String dateOfBirth;
+    private String email;
+
+    public UserDetail(Integer id, String name, String dateOfBirth, String email) {
+        this.id = id;
+        this.name = name;
+        this.dateOfBirth = dateOfBirth;
+        this.email = email;
+    }
+    public Integer getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+} 
