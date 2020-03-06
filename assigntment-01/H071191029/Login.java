659047a930a588c91682916254602ee6ee6fab80
annisaputriaulia <ao7901211@gmail.com>
2020-03-07 00:14:43 +0800

    tugas pbo

---
+++ assigntment-01/H071191029/Login.java
@@ -0,0 +1,40 @@
+import java.util.NoSuchElementException;
+
+class Login {
+    static Login instance = null;
+    private User user;
+    private UserDetail userDetail;
+    private DataSource dataSource;
+
+    private Login() {
+
+    }
+
+    static public Login getInstance() {
+        if (instance == null)
+            instance = new Login();
+        return instance;
+    }
+
+    public void auth(String name, String pass) throws NoSuchElementException {
+        dataSource = DataSource.getInstance();
+        try {
+            user = dataSource.getUser(name);
+            if (user.getPassword().equals(pass)) {
+                userDetail = user.getUserDeteail();
+            } else {
+                System.out.println("wrong password");
+            }
+        } catch (Exception e) {
+            throw new NoSuchElementException("user not found: " + name);
+        }
+    }
+
+    public void status() {
+        if (userDetail != null) {
+            System.out.println(userDetail.getName());
+            System.out.println(userDetail.getEmail());
+            System.out.println(userDetail.getDateOfBirth());
+        }
+    }
+}
