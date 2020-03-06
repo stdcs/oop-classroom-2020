82dc2083b74bdca37b8fa41b146c440a3f29c05a
Septi <54713592+septiintanaml@users.noreply.github.com>
2020-03-06 23:54:43 +0800

    Update Main.java

--- assignment-02/H071191022/Main.java
+++ assignment-02/H071191022/Main.java
@@ -1,8 +1,7 @@
-public class Main {
-
-    public static void main(String[] args) {
-        Login login = new Login();
-        login.auth("fitrh","12345678");
+class Main {
+    public static void main(String[] args) throws Exception {
+        Login login = Login.getInstance();
+        login.auth("naim", "Naim");
         login.status();
     }
-}
+} 
