9218fb71ecd67f693b5d8fdeba93a7627815d01a
Nurul Salsabila Syam <54732798+salsaasy@users.noreply.github.com>
2020-03-06 14:51:45 +0000

    Delete App.java

--- assignment-02/H071191054/App.java
+++
@@ -1,7 +0,0 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        Login login = new Login();
-        login.auth("naim", "Naim");
-        login.status();
-    }
-}
