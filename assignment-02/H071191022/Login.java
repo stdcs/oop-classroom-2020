b04fb388e5919e14da29a934580488ec6d342f70
Septi <54713592+septiintanaml@users.noreply.github.com>
2020-03-06 23:53:52 +0800

    Update Login.java

--- assignment-02/H071191022/Login.java
+++ assignment-02/H071191022/Login.java
@@ -1,28 +1,40 @@
-class Login {
+import java.util.NoSuchElementException;
 
-    private User user;
-    private UserDetail userDetail;
-    private DataSource dataSource;
+ class Login {
+     static Login instance = null;
+     private User user;
+     private UserDetail userDetail;
+     private DataSource dataSource;
 
-    public void auth (String name,String pass){
-        dataSource = new DataSource();
-        if ((user = dataSource.getUser(name)) != null){
-            dataSource = new DataSource();
-            if ((user = dataSource.getUser(name))!= null){
-                userDetail = user.getUserDetail();
-            }else{
-                System.out.println("wrong password");
-            }
-        }else{
-            System.out.println("user not found");
-        }
-    }
+     private Login() {
 
-    public void status(){
-        if (userDetail != null){
-            System.out.println(userDetail.getName());
-            System.out.println(userDetail.getEmail());
-            System.out.println(userDetail.getDateOfBirth());
-        }
-    }
-}
+     }
+
+     static public Login getInstance() {
+         if (instance == null)
+             instance = new Login();
+         return instance;
+     }
+
+     public void auth(String name, String pass) throws NoSuchElementException {
+         dataSource = DataSource.getInstance();
+         try {
+             user = dataSource.getUser(name);
+             if (user.getPassword().equals(pass)) {
+                 userDetail = user.getUserDetail();
+             } else {
+                 System.out.println("wrong password");
+             }
+         } catch (Exception e) {
+             throw new NoSuchElementException("user not found: " + name);
+         }
+     }
+
+     public void status() {
+         if (userDetail != null) {
+             System.out.println(userDetail.getName());
+             System.out.println(userDetail.getEmail());
+             System.out.println(userDetail.getDateOfBirth());
+         }
+     }
+ } 
