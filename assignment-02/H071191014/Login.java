2772afc86293ffc52b90e1788d1b413d6674b83d
Andiqal <55188362+Andiqal@users.noreply.github.com>
2020-03-06 18:37:20 +0700

    H071191014
    assignment-02

---
+++ assignment-02/H071191014/Login.java
@@ -0,0 +1,36 @@
+import java.io.IOException;
+
+class Login{
+    private User user;
+    private UserDetail userDetail;
+    private DataSource dataSource;
+    boolean is_accesible = false;
+    static Login login = new Login();
+
+
+    private Login(){}
+
+    static Login getObject(){
+        return login;
+    }
+
+    public void auth(String userName, String password) throws IOException{
+        dataSource = DataSource.getObject();
+        user = dataSource.getUser(userName);
+        is_accesible = password.equals(user.getPassword()) ? true:false;
+        userDetail = is_accesible ? dataSource.getUserDetail(user.getId()):null;
+
+    }
+
+    public void status(){
+        if(userDetail == null){
+            System.out.println("NoSuchElementException");
+        }else{
+            System.out.println("Name            : " + userDetail.getName());
+            System.out.println("Email           : " + userDetail.getEmail());
+            System.out.println("Date of Birth   : " + userDetail.getBirth());
+            System.out.println("UserName        : " + user.getUserName());
+            System.out.println("Password        : " + user.getPassword());
+        }
+    }
+}
---
+++ assignment-02/H071191014/Main.java
@@ -0,0 +1,10 @@
+import java.io.IOException;
+
+class Main{
+    public static void main(String[] args) throws IOException{
+        Login login = Login.getObject();
+
+        login.auth("farhan", "Parhan");
+        login.status();
+    }
+}
